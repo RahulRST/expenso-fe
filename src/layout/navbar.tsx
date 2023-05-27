5d57d3cac2f8d43bfd927b99b88705e59f4cfaef
RahulRST <srahuljayaraj@gmail.com>
2023-05-27 15:24:23 +0530

    responsive

--- src/layout/navbar.tsx
+++ src/layout/navbar.tsx
@@ -1,51 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import Logo from "../assets/logo.svg"
+import Logo from '../assets/logo.svg';
 
 const Navbar: React.FC = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleMenuToggle = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const handleLogout = () => {
+    sessionStorage.clear();
+    navigate('/');
+  };
+
   return (
     <nav className="bg-gray-900">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
             <div className="flex-shrink-0">
-            <Link to="/home">
-              <img
-                className="h-20 w-20"
-                src={Logo}
-                alt="Logo"
-              />
+              <Link to="/home">
+                <img className="h-20 w-20" src={Logo} alt="Logo" />
               </Link>
             </div>
-            <div className="hidden md:block">
-              <div className="ml-10 flex items-baseline space-x-4">
+            <div className="hidden md:block ml-10">
+              <div className="flex space-x-4">
                 <Link
-                  to="/expense" 
+                  to="/expense"
                   className="text-gray-300 hover:bg-orange-500 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                 >
                   Expense Tracker
                 </Link>
                 <Link
-                  to="/income" 
+                  to="/income"
                   className="text-gray-300 hover:bg-orange-500 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                 >
                   Income Tracker
                 </Link>
                 <Link
-                  to="/notifications" 
+                  to="/notifications"
                   className="text-gray-300 hover:bg-orange-500 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                 >
                   Notifications
                 </Link>
                 <Link
-                  to="/reminder" 
-                  className="text-gray-300 hover:bg-orange-500 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                >
-                  Reminder
-                </Link>
-                <Link
-                  to="/analytics" 
+                  to="/analytics"
                   className="text-gray-300 hover:bg-orange-500 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                 >
                   Analytics
@@ -53,16 +54,76 @@ const Navbar: React.FC = () => {
               </div>
             </div>
           </div>
+          <div className="flex items-center md:hidden">
+            <button
+              onClick={handleMenuToggle}
+              className="text-gray-300 hover:text-white px-4 focus:outline-none"
+            >
+              <svg
+                className="h-6 w-6"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                {isMenuOpen ? (
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                ) : (
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                )}
+              </svg>
+            </button>
+          </div>
           <div className="flex items-center">
-            <a
-              onClick={() => {navigate("/")}}
+            <button
+              onClick={handleLogout}
               className="text-gray-300 hover:text-white px-4"
             >
               Logout
-            </a>
+            </button>
           </div>
         </div>
       </div>
+      {isMenuOpen && (
+        <div className="md:hidden bg-gray-900">
+          <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+            <Link
+              to="/expense"
+              className="text-gray-300 hover:bg-orange-500 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+            >
+              Expense Tracker
+            </Link>
+            <Link
+              to="/income"
+              className="text-gray-300 hover:bg-orange-500 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+            >
+              Income Tracker
+            </Link>
+            <Link
+              to="/notifications"
+              className="text-gray-300 hover:bg-orange-500 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+            >
+              Notifications
+            </Link>
+            <Link
+              to="/analytics"
+              className="text-gray-300 hover:bg-orange-500 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+            >
+              Analytics
+            </Link>
+          </div>
+        </div>
+      )}
     </nav>
   );
 };
--- src/pages/analytics.tsx
+++ src/pages/analytics.tsx
@@ -7,47 +7,20 @@ Chart.register(BarElement, LinearScale, CategoryScale);
 
 const Analytics: React.FC = () => {
   
-  const [categories, setCategories] = useState<string[]>([]);
-  const [amounts, setAmounts] = useState<number[]>([]);
-
-  let data: any = {
-    labels: [],
-    datasets: [
-      {
-        label: 'Budget Progress',
-        data: [],
-        backgroundColor: 'rgba(255, 159, 64, 0.8)',
-      },
-    ],
-  };
+  const [ expenseData, setExpenseData ] = useState<any[]>([]);
+  const [ incomeData, setIncomeData ] = useState<any[]>([]);
 
   useEffect(() => {
     const getBudget = async () => {
-      const budget = await axios.get(import.meta.env.VITE_BACKEND_URL+"/analysis", {
+      const budget = await axios.get(import.meta.env.VITE_BACKEND_URL+"/fetch/analytics", {
         headers: {
           "Authorization": "Bearer " + sessionStorage.getItem("expenso_token")
         }
       })
       if(budget.data.success){
         console.log("Budget fetched successfully",budget.data)
-        const labels = []
-        const datasets = []
-        budget.data.expenses.forEach((expense: any) => {
-          labels.push(expense.description)
-          datasets.push(expense.amounts)
-        })
-        // const renderExpenses = {
-        //   const data = {
-        //     labels: expense.description,
-        //     datasets: [
-        //       {
-        //         label: 'Budget Progress',
-        //         data: expense.amounts,
-        //         backgroundColor: 'rgba(255, 159, 64, 0.8)',
-        //       },
-        //   }
-        // setCategories(budget.data.budget.categories)
-        // setAmounts(budget.data.budget.amounts)
+        setExpenseData(budget.data.expenses)
+        setIncomeData(budget.data.incomes)
       }
       else{
         console.log("Error fetching budget")
@@ -56,28 +29,102 @@ const Analytics: React.FC = () => {
     getBudget()
   }, [])
 
+  const ExpenseChart = () => {
 
-  const options = {
-    scales: {
-      y: {
-        beginAtZero: true,
-        max: 100,
-        ticks: {
-          stepSize: 20,
+    let categories: any = [];
+    let amounts: any = [];
+    let maxAmount = 0;
+
+    expenseData.forEach((expense: any) => {
+      if(expense.amount > maxAmount){
+        maxAmount = expense.amount
+      }
+      categories.push(expense.description)
+      amounts.push(expense.amount)
+    })
+
+    const data = {
+      labels: categories,
+      datasets: [
+        {
+          label: 'Expense Progress',
+          data: amounts,
+          backgroundColor: 'rgba(255, 159, 64, 0.8)',
+        },
+      ],
+    };
+
+    const options = {
+      scales: {
+        y: {
+          beginAtZero: true,
+          max: maxAmount,
+          ticks: {
+            stepSize: maxAmount/10,
+          },
         },
       },
-    },
-  };
+    };
+
+    return (
+      <div className="w-full">
+        <h3 className="text-lg font-bold mb-4">Expense Progress</h3>
+        <Bar data={data} options={options} />
+      </div>
+    );
+  }
+
+  const IncomeChart = () => {
+
+    let categories: any = [];
+    let amounts: any = [];
+    let maxAmount = 0;
+    incomeData.forEach((income: any) => {
+      if(income.amount > maxAmount){
+        maxAmount = income.amount
+      }
+      categories.push(income.description)
+      amounts.push(income.amount)
+    })
+
+    const data = {
+      labels: categories,
+      datasets: [
+        {
+          label: 'Income Progress',
+          data: amounts,
+          backgroundColor: 'rgba(75, 192, 192, 0.8)',
+        },
+      ],
+    };
+
+    const options = {
+      scales: {
+        y: {
+          beginAtZero: true,
+          max: maxAmount,
+          ticks: {
+            stepSize: maxAmount/10,
+          },
+        },
+      },
+    };
+    
+    return (
+      <div className="w-full">
+        <h3 className="text-lg font-bold mb-4">Income Progress</h3>
+        <Bar data={data} options={options} />
+      </div>
+    );
+  }
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white">
       <div className="max-w-md w-full p-6 bg-gray-800 rounded-lg shadow-lg">
-        <h2 className="text-3xl text-orange-500 font-bold mb-6">Budget Analytics</h2>
+        <h2 className="text-3xl text-orange-500 text-center font-bold mb-6">Budget Analytics</h2>
         <div className="mt-8">
-          <h3 className="text-lg font-bold mb-4">Budget Progress</h3>
-          <div className="w-full">
-            <Bar data={data} options={options} />
-          </div>
+          <ExpenseChart />
+          <IncomeChart />
         </div>
       </div>
     </div>
